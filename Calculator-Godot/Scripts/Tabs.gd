ac0ca886f9957eababba4e48439583b82c02b1f9
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2020-11-26 15:13:50 -0500

    Added Main Menu tabs

--- Calculator-Godot/Scripts/Tabs.gd
+++ Calculator-Godot/Scripts/Tabs.gd
@@ -13,6 +13,13 @@ func ButtonPressed(node):
 		allButton[i].get_node("Name").modulate = themeManager.textColor
 		allButton[i].get_node("Name/ColorTag").colorId = 6
 	
+	var allTabs = get_parent().get_parent().get_children()
+	for i in range(allTabs.size()):
+		if (allTabs[i].name != node.name && allTabs[i].name != "Tabs"):
+			allTabs[i].set_visible(false)
+		else:
+			allTabs[i].set_visible(true)
+	
 	node.get_node("Name/ColorTag").colorId = 7
 	node.get_node("Name").modulate = themeManager.textHighlightColor
 	node.get_node("Background").set_visible(true)
